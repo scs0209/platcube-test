dbac46a2c67c15875a603f6365bcc20453be527e
scs0209 <110822847+scs0209@users.noreply.github.com>
2024-01-18 19:33:26 +0900

    feat: apollo client 초기 셋팅 완료 및 seeNotices 데이터 연결

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,30 @@
-export default function HomePage() {
+import { getClient } from "@/lib/client";
+import { gql } from "@apollo/client";
+
+const query = gql`
+  query SeeHospitalNoticeInfo($take: Int, $cursor: Int) {
+    seeNotices(take: $take, cursor: $cursor) {
+      noticeList {
+        createdAt
+        creatorImg
+        name
+        ntc_id
+        rank
+        text
+        title
+      }
+      totalLength
+    }
+  }
+`;
+
+export default async function HomePage() {
+  const { data, error } = await getClient().query({
+    query,
+    variables: { take: 10, cursor: 0 },
+  });
+
+  console.log(JSON.stringify(data, null, 2));
+
   return <main>HomePage</main>;
 }
