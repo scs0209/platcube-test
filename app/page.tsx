0b793e98d5cabc691bdddbe96b3f09ccd069c7e8
scs0209 <110822847+scs0209@users.noreply.github.com>
2024-01-18 19:46:17 +0900

    feat: seeschedul, seestatics까지 연결 완료

--- app/page.tsx
+++ app/page.tsx
@@ -18,13 +18,58 @@ const query = gql`
   }
 `;
 
+const query2 = gql`
+  query SeeSchedule($searchDate: String!) {
+    seeSchedule(searchDate: $searchDate) {
+      roomName
+      drName
+      drRank
+      subDrUsed
+      isOffDay
+      offStartHour
+      offStartMin
+      offEndHour
+      offEndMin
+      startHour
+      startMin
+      endHour
+      endMin
+      lunchBreak
+      lbStartHour
+      lbStartMin
+      lbEndHour
+      lbEndMin
+    }
+  }
+`;
+
+const query3 = gql`
+  query SeeStatisticsInfo {
+    seeStatistics {
+      byWeekNumberCount {
+        weekNumber
+        count
+      }
+      manCount
+      womanCount
+    }
+  }
+`;
+
 export default async function HomePage() {
   const { data, error } = await getClient().query({
     query,
     variables: { take: 10, cursor: 0 },
   });
+  const { data: scheduleData } = await getClient().query({
+    query: query2,
+    variables: { searchDate: "2022-01-01" },
+  });
+  const { data: staticsInfo } = await getClient().query({
+    query: query3,
+  });
 
-  console.log(JSON.stringify(data, null, 2));
+  console.log(JSON.stringify(staticsInfo, null, 2));
 
   return <main>HomePage</main>;
 }
